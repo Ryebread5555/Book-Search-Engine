8ebcec325b1e8ed071ebd2e90b78416443454073
Ryebread5555 <Rypetty55@gmail.com>
2023-04-30 20:33:58 -0500

    updated server

--- server/server.js
+++ server/server.js
@@ -5,39 +5,41 @@ const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-// Create Apollo server and pass schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers,
-  context: authMiddleware
+  resolvers, 
+  context: authMiddleware,
+  persistedQueries: false, 
 });
-
-async function startServer() {
-  await server.start();
-  server.applyMiddleware({ app });
-}
-
-startServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  // add a wildcard route at the end of the routes to serve the index.html file
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
 
-db.once('open', async () => {
-  await startServer();
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`Server now running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+    });
   });
-});
+};
+
+// start server
+startApolloServer(typeDefs, resolvers);
+
+
